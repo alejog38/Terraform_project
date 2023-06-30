39cda373eb2830e56eda5c112f632cd83f6b3eac
Alejandro Gonzalez <alejog38@gmail.com>
2023-06-30 13:15:06 -0500

    outputs file updated

--- outputs.tf
+++ outputs.tf
@@ -8,15 +8,6 @@ output "ecs_cluster_arn" {
   value       = aws_ecs_cluster.ecs_cluster.arn
 }
 
-output "subnet_private" {
-  value = [for s in data.aws_subnet.private : s.id]
-}
-
-output "subnet_public" {
-  value = [for s in data.aws_subnet.public : s.id]
-}
-
-
 output "load_balancer_ip" {
   value = aws_lb.default.dns_name
 }
